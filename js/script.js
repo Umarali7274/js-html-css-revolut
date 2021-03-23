027da2c6b6fd77629f0abb23b12559ebc32756dc
michele <micheleepedrotti@gmail.com>
2021-03-23 17:57:52 +0100

    ex ok

--- js/script.js
+++ js/script.js
@@ -8,13 +8,13 @@ function openThisDrodown(){
     $('.dropdown').removeClass('js-flex');
     
     // hovered dropdown shows
-    active.find('.dropdown').addClass('js-flex')
+    active.find('.dropdown').addClass('js-flex');
 
     // disable click event on dropdown link
     active.click(function(e){
 
         e.stopPropagation()
-    })
+    });
 };
 // close dropdown by clicking elsewhere
 function removeDropdown(){
@@ -24,9 +24,9 @@ function removeDropdown(){
 function init(){
 
     // dropdown on hover
-    $('#features, #plans, #about, #help').hover(openThisDrodown)
+    $('#features, #plans, #about, #help').hover(openThisDrodown);
     // elsewhere click event
-    $(document).click(removeDropdown)
+    $(document).click(removeDropdown);
 };
 
 $('document').ready(init);
