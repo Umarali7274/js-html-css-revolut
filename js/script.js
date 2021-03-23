453fa3f02b883d68e5ce96f7cfca494b1450521b
michele <micheleepedrotti@gmail.com>
2021-03-23 16:57:33 +0100

    dropdowns shows on hover and hide on page click

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
+// selected dropdown shows - other open dropdown hide
+function openDrodown(){
+
+    $('.dropdown').removeClass('js-flex');
+    $(this).find('.dropdown').addClass('js-flex')
+    
+
+};
+// close dropdown by clicking elsewhere
+function removeDropdown(){
+
+    $('.dropdown').removeClass('js-flex');
+}
+
 function init(){
 
+    // dropdown on hover
+    $('#features, #plans, #about, #help').hover(openDrodown)
+    // elsewhere click event
+    $(document).click(removeDropdown)
 };
 
 $('document').ready(init);
